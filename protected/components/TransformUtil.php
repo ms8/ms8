8bd88aae744c26843fbdce92d5ddf50cc849e9a2
syd3050 <syd3050@163.com>
2013-09-26 22:50:13 +0800

    对象转换工具类

---
+++ protected/components/TransformUtil.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: syd3050
+ * Date: 13-9-26
+ * Time: 下午10:21
+ * To change this template use File | Settings | File Templates.
+ */
+
+class TransformUtil {
+    public static function arrayToObject($e){
+        if( gettype($e)!='array' ) return;
+        foreach($e as $k=>$v){
+            if( gettype($v)=='array' || getType($v)=='object' )
+                $e[$k]=(object)arrayToObject($v);
+        }
+        return (object)$e;
+    }
+
+    public static function objectToArray($e){
+        $e=(array)$e;
+        foreach($e as $k=>$v){
+            if( gettype($v)=='resource' ) return;
+            if( gettype($v)=='object' || gettype($v)=='array' )
+                $e[$k]=(array)objectToArray($v);
+        }
+        return $e;
+    }
+}
