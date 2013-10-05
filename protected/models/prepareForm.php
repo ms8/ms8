eaa6a52c0e549fd0af8425719fde4c577cac232f
syd3050 <syd3050@163.com>
2013-10-05 09:49:30 +0800

    修改面试准备对应前端展现的类，新增一些字段

--- protected/models/prepareForm.php
+++ protected/models/prepareForm.php
@@ -12,24 +12,19 @@ class PrepareForm {
     public $username;
     public $picPath;
     public $prepareID;
-    public $title;
     public $time;
+    public $address;
+    public $companyName;
+    public $position;
 
-    /**
-     * 构造函数，new prepareForm(...)时调用
-     * @param $userID
-     * @param $username
-     * @param $picPath
-     * @param $prepareID
-     * @param $title
-     * @param $time
-     */
-    function __construct($userID,$username,$picPath,$prepareID,$title,$time) {
+    public function __construct($userID,$username,$picPath,$prepareID,$time,$address,$companyName,$position){
         $this->userID = $userID;
         $this->username = $username;
         $this->picPath = $picPath;
         $this->prepareID = $prepareID;
-        $this->title = $title;
         $this->time = $time;
+        $this->address = $address;
+        $this->companyName = $companyName;
+        $this->position = $position;
     }
 }
