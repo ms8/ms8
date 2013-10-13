eb7f0824d61d0278e84d9b327b62f791a7133568
guhuazhen <gqlshare@163.com>
2013-10-13 14:47:38 +0800

    ghz temp

--- protected/config/main.php
+++ protected/config/main.php
@@ -69,7 +69,7 @@ return array(
 		// uncomment the following to use a MySQL database
 
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=ms8',
+			'connectionString' => 'mysql:host=localhost;dbname=ms',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '1',
