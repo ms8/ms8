1c9143fee077dffd4c98a452d95e4494e1be37f2
syd3050 <syd3050@163.com>
2013-10-21 22:32:47 +0800

    本地修改

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 //phpinfo();
 // change the following paths if necessary
-//$yii=dirname(__FILE__).'/framework/yii.php';
-//$config=dirname(__FILE__).'/protected/config/main.php';
-$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+$yii=dirname(__FILE__).'/framework/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
+//$yii=dirname(__FILE__).'/../yii/framework/yii.php';
+//$config=dirname(__FILE__).'/protected/config/main.php';
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
