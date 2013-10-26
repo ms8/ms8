4dfd874ecc7721a1d100b21e00a29351e0be7c7e
guhuazhen <gqlshare@163.com>
2013-10-26 10:28:43 +0800

    ghz

---
+++ protected/views/selfIntroduction/_viewcomment.php
@@ -0,0 +1,19 @@
+<div class="view">
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('posterName')); ?>:</b>
+	<?php echo CHtml::encode($data->posterName); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('content')); ?>:</b>
+	<?php echo CHtml::encode($data->content); ?>
+	<br />
+
+	<b><?php echo CHtml::encode($data->getAttributeLabel('time')); ?>:</b>
+	<?php echo CHtml::encode($data->time); ?>
+	<br/>
+    <a href="/ms8/ms8/index.php?r=SelfIntroductionComment/create&intro_id=<?php echo CHtml::encode($data->intro_id);?>&toComment=<?php echo CHtml::encode($data->commentID);?>">
+        <b>回复</b>
+    </a>
+    </br>
+
+</div>
