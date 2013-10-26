75259c9630c2116da8f6cfa3215797e00a8fce70
ms8 <gqlshare@163.com>
2013-10-26 11:11:26 +0800

    准备面试 搜索结果 收藏按钮修改

--- protected/views/prepare/_search2.php
+++ protected/views/prepare/_search2.php
@@ -1,4 +1,4 @@
 <ul class="unstyled" style="line-height: 30px;height:30px;">
-    <?php echo CHtml::link(CHtml::encode($data['title']),$data['url'],array("target"=>"_blank")); ?>
-    <!--button class="btn btn-primary " style="float: right">收藏</button-->
+    <div style="float: left"><?php echo CHtml::link(CHtml::encode($data['title']),$data['url'],array("target"=>"_blank")); ?></div>
+    <button class='btn btn-primary' style='float:left;margin-left:20px;display: none;'>收藏</button>
 </ul>
