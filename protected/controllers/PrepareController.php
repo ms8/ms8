fd8f0bf1d48dad79cd29934434dbbb6e8d7250e4
syd3050 <syd3050@163.com>
2013-11-23 18:53:52 +0800

    面试总结样式修改

--- protected/controllers/PrepareController.php
+++ protected/controllers/PrepareController.php
@@ -26,7 +26,7 @@ class PrepareController extends Controller
     {
         return array(
             array('allow',  // allow all users to perform 'index' and 'view' actions
-                'actions'=>array('index','view',"save","interview"),
+                'actions'=>array('index','view',"save","interview","list"),
                 'users'=>array('*'),
             ),
             array('allow', // allow authenticated user to perform 'create' and 'update' actions
@@ -282,7 +282,8 @@ class PrepareController extends Controller
         $dataInterview = new CArrayDataProvider($ms);
         $this->render('interview',array(
             'dataInterview'=>$dataInterview,
-            'userName'=>""
+            'userName'=>"",
+            'prepareID'=>"",
         ));
     }
 }
